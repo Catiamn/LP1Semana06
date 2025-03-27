1aa126d634dae15339f1494d7b13fbe71b768f6b
Cátia Nascimento <catia.nascimento2006@gmail.com>
2025-03-27 21:07:44 +0000

    Added a system to add up the dice numbers

--- RandDice/Program.cs
+++ RandDice/Program.cs
@@ -6,7 +6,19 @@ namespace RandDice
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            //total dos dados lançados que depois vai aparecer na console
+            int total = 0;
+            //second argument = seed
+            Random s = new Random(int.Parse(args[1]));
+
+            //first argument = number of times the dice is gonna be rolled
+            for (int i = 0; i < int.Parse(args[0]); i++)
+            {
+                //the dice can roll between 1-6
+                total += s.Next(1,7);
+            }
+            Console.WriteLine(total);
+
         }
     }
 }
