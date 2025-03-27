764f743399896ad0e2d533b6a01f408a5911f462
Cátia Nascimento <catia.nascimento2006@gmail.com>
2025-03-27 23:33:23 +0000

    Added Information about each enemy

---
+++ MyGame/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyGame
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            int NumberEnemy = int.Parse(args[0]);
+
+            Enemy[] inimigos = new Enemy[NumberEnemy];
+
+            for(int i = 0; i < NumberEnemy; i++)
+            {
+                Console.Write($"Nome do inimigo {i+1}:");
+                string nome = Console.ReadLine();
+                inimigos[i] = new Enemy(nome);
+            }
+
+            for(int i = 0; i < NumberEnemy; i++)
+            {
+                    Console.Write($"{inimigos[i].GetName()} {inimigos[i].GetHealth()} {inimigos[i].GetShield()}");
+                    Console.WriteLine();
+            }
+        }
+    }
+
+}
