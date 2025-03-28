71b14a6e86610ab854180466e9ca6a4d415ea88f
Cátia Nascimento <catia.nascimento2006@gmail.com>
2025-03-28 14:02:04 +0000

    Fixed some erros

--- MyRPG/Program.cs
+++ MyRPG/Program.cs
@@ -2,25 +2,60 @@
  
  namespace MyRPG
  {
-     public class Program
+     public class MyRPG
      {
+        public enum Hardness
+     {
+         Easy,
+         Normal,
+         Hard
+     }
+     
+         private string name;
+         private float health;
+         private float shield;
+         public MyRPG(string name)
+         {
+             this.name = name;
+             health = 100;
+             shield = 0;
+         }
+ 
+         public string GetName()
+         {
+             return name;
+         }
+ 
+         public void TakeDamage(float damage)
+         {
+             shield -= damage;
+             if (shield < 0)
+             {
+                 float damageStillToInflict = -shield;
+                 shield = 0;
+                 health -= damageStillToInflict;
+                 if (health < 0) health = 0;
+             }
+         }
+
          private static void Main()
          {
-             GameLevel gl = new GameLevel(104, Hardness.Normal);
+     
+            GameLevel gl = new GameLevel(104, Hardness.Normal);
  
-             gl.SetEnemyInRoom(1, new Enemy("Worf"));
-             gl.SetEnemyInRoom(7, new Enemy("Picard"));
-             gl.SetEnemyInRoom(16, new Enemy("Data"));
-             gl.SetEnemyInRoom(94, new Enemy("Riker"));
-             gl.SetEnemyInRoom(59, new Enemy("Troi"));
+            gl.SetEnemyInRoom(1, new Enemy("Worf"));
+            gl.SetEnemyInRoom(7, new Enemy("Picard"));
+            gl.SetEnemyInRoom(16, new Enemy("Data"));
+            gl.SetEnemyInRoom(94, new Enemy("Riker"));
+            gl.SetEnemyInRoom(59, new Enemy("Troi"));
  
-             Console.WriteLine($"Difficulty: {gl.GetHardness()}");
+            Console.WriteLine($"Difficulty: {gl.GetHardness()}");
  
-             Console.WriteLine($"Number of rooms: {gl.GetNumRooms()}");
+            Console.WriteLine($"Number of rooms: {gl.GetNumRooms()}");
  
-             Console.WriteLine($"Number of enemies: {gl.GetNumEnemies()}");
+            Console.WriteLine($"Number of enemies: {gl.GetNumEnemies()}");
  
-             gl.PrintEnemies();
+            gl.PrintEnemies();
  
              // Este programa mostra o seguinte no ecrã:
              //
