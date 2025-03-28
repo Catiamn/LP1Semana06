9f18bde25be98a0f8da1f99f556ef4924ca6d6d8
Cátia Nascimento <catia.nascimento2006@gmail.com>
2025-03-28 14:31:39 +0000

    Added GameLevel information

--- MyRPG/GameLevel.cs
+++ MyRPG/GameLevel.cs
@@ -1,11 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
 using Humanizer;
 
- 
  namespace MyRPG
  {
      public class GameLevel
      {
-        private Hardness = 
+        private Hardness difficulty;
+        public Hardness GetHardness() => difficulty
+
+        private int numberRoom;
+
+        public int GetNumRooms() => numberRoom;
+        
+        private List<Enemy> enemies;
+        private int numenemies = 0;
+        public int GetNumEnemies() => numenemies;
+
+        public GameLevel(int numberRoom, MyRPG.Hardness difficulty)
+        {
+            this.difficulty = difficulty;
+            this.numberRoom = numberRoom;
+            enemies = new List<Enemy>();
+            for (int i = 0; i < numberRoom; i++)
+            {
+                enemies.Add(null);
+            }
+        }
+
+
+
      }
  }
