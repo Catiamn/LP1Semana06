004533d1f9ccfc68a58dc776dec8cab33630b1c0
Cátia Nascimento <catia.nascimento2006@gmail.com>
2025-03-28 13:55:52 +0000

    Added Program

---
+++ MyRPG/MyRPG.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MyRPG
+{
+    public class MyRPG
+    {
+        
+        public enum Hardness
+     {
+         Easy,
+         Normal,
+         Hard
+     }
+     
+         private string name;
+         private float health;
+         private float shield;
+         public MyRPG(string name)
+         {
+             this.name = name;
+             health = 100;
+             shield = 0;
+         }
+ 
+         public string GetName()
+         {
+             return name;
+         }
+ 
+         public void TakeDamage(float damage)
+         {
+             shield -= damage;
+             if (shield < 0)
+             {
+                 float damageStillToInflict = -shield;
+                 shield = 0;
+                 health -= damageStillToInflict;
+                 if (health < 0) health = 0;
+             }
+         }
+     }
+ }
